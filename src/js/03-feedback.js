9c8b0f1ee17512d38dfd1ce0b7725b67a18fff24
unknown <90862670+Vadim-kaR@users.noreply.github.com>
2022-04-26 11:30:37 +0300

    task 3 done ver 0.1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,62 @@
+const refs = {
+  fbForm:document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+  textArea:document.querySelector('.feedback-form textarea')
+}
 
+
+refs.fbForm.addEventListener('submit', onFormSubmit);
+refs.input.addEventListener('input', onFormInput);
+refs.textArea.addEventListener('input' , onFormTextAreaInput)
+
+const INPUT_STORAGE_KEY = "feedback-form-state";
+const obj = {}
+
+
+
+function onFormInput(e) { 
+    const value = e.target.value;
+    obj.email = value;
+    localStorage.setItem(INPUT_STORAGE_KEY, JSON.stringify(obj))
+}
+
+function onFormTextAreaInput(e) { 
+    const text = e.target.value;
+    obj.message = text;
+    localStorage.setItem(INPUT_STORAGE_KEY, JSON.stringify(obj))
+}
+
+function getStorageInfo() { 
+    const storageInfo = localStorage.getItem(INPUT_STORAGE_KEY);
+    return JSON.parse(storageInfo);
+}
+
+const storageValue = getStorageInfo();
+    
+
+function InfoFromStorege() {
+
+    if (storageValue){
+        refs.input.value = storageValue.email;
+        refs.textArea.value = storageValue.message;
+    }
+}
+
+InfoFromStorege();
+
+function consoleObj() { 
+    if (Object.keys(obj).length == 0) {
+        console.log(storageValue);
+    }
+    else { 
+        console.log(obj)
+    }
+}
+
+function onFormSubmit(e) { 
+
+    e.preventDefault();
+    consoleObj();
+    localStorage.removeItem(INPUT_STORAGE_KEY);
+    e.currentTarget.reset();
+}
